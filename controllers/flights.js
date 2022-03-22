5b9e20de1e13843fac9b8a892f241e909a337912
BRAKEL3SS <rck496@yahoo.com>
2022-03-22 16:00:35 -0700

    implemented header and footer partials with navbar

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,17 +8,14 @@ module.exports = {
 
 function newFlight(req, res) {
     const newFlight = new Flight();
-    // Obtain the default date
-    const dt = newFlight.departs;
-    // Format the date for the value attribute of the input
-    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
-    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
-    res.render('flights/new', { title: 'New Flight', departsDate });
+// Obtain the default date
+const dt = newFlight.departs;
+// Format the date for the value attribute of the input
+let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+res.render('flights/new', { title: 'New Flight', departsDate });
 }
 function create(req, res) {
-    for (let key in req.body) {
-        console.log(req.body.key);
-    }
     const flight = new Flight(req.body)
     flight.save(function(err) {
         if (err) return res.render('flights/new')
