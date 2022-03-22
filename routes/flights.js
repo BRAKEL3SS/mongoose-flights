4131cd7fac109767b5ee29bb331ede90f41d567f
BRAKEL3SS <rck496@yahoo.com>
2022-03-22 15:06:41 -0700

    added create functionality

--- routes/flights.js
+++ routes/flights.js
@@ -4,5 +4,6 @@ const flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
 router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
 
 module.exports = router;
