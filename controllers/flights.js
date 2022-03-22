a12b82e637691f9d8498b77dbbfe7c899f305961
BRAKEL3SS <rck496@yahoo.com>
2022-03-22 15:23:23 -0700

    added index functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,10 +3,11 @@ const Flight = require('../models/flight')
 module.exports = {
     new: newFlight,
     create,
+    index,
 }
 
 function newFlight(req, res) {
-    res.render('flights/new')
+    res.render('flights/new', { title: 'Add Flight' })
 }
 function create(req, res) {
     const flight = new Flight(req.body)
@@ -14,5 +15,10 @@ function create(req, res) {
         if (err) return res.render('flights/new')
         console.log(flight)
         res.redirect('/flights/new')
+    })
+}
+function index(req, res) {
+    Flight.find({}, function(err, flights) {
+        res.render('flights/index', { title: 'All Flights', flights })
     })
 }
