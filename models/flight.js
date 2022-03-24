679a6990e3893bb840980cdb9ba40bdcffd5410a
BRAKEL3SS <rck496@yahoo.com>
2022-03-23 21:04:52 -0700

    added destinations to show page including create functionality

--- models/flight.js
+++ models/flight.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const destinationSchema = new Schema({
-    airport: {type:String, default: 'DEN',
+    airport: {type:String,
     enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
     arrival: Date
 })
